ac6891a72839cf6597e2425c36c3be0f3a808aea
Luis <luis901101@gmail.com>
2021-09-13 15:53:39 -0400

    - Android plugin API updated to support v2 Embedding while remaining compatible with apps that don’t use the v2 Android embedding.

--- android/src/main/java/com/plugin/flutter/zkcscanner/ZkcScannerPlugin.java
+++ android/src/main/java/com/plugin/flutter/zkcscanner/ZkcScannerPlugin.java
@@ -1,25 +1,48 @@
 package com.plugin.flutter.zkcscanner;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/**
- * Created by luis901101 on 05/30/19.
- */
+/** Created by luis901101 on 05/30/19. /
 
 /** ZKCScannerPlugin */
-public class ZkcScannerPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    new ZkcScannerPlugin(registrar);
+public class ZkcScannerPlugin implements FlutterPlugin, MethodCallHandler {
+
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    init(
+        flutterPluginBinding.getApplicationContext(),
+        flutterPluginBinding.getBinaryMessenger()
+    );
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    if(channel != null) channel.setMethodCallHandler(null);
+  }
+
+  // This static method is only to remain compatible with apps that don’t use the v2 Android embedding.
+  @Deprecated()
+  @SuppressLint("Registrar")
+  public static void registerWith(Registrar registrar)
+  {
+    new ZkcScannerPlugin().init(
+        registrar.context(),
+        registrar.messenger()
+    );
   }
 
   static final String _METHOD_CHANNEL = "zkcscanner";
@@ -30,7 +53,7 @@ public class ZkcScannerPlugin implements MethodCallHandler {
   static final String _ON_DECODED = "onDecoded";
   static final String _ON_ERROR = "onError";
 
-  class Constants {
+  static class Constants {
     public static final String ACTION_SCAN_CODE_CALLBACK = "com.zkc.scancode";
     public static final String ACTION_KEY_CODE = "com.zkc.keycode";
     public static final String ACTION_OPEN = "android.intent.action.SCREEN_ON";
@@ -69,17 +92,21 @@ public class ZkcScannerPlugin implements MethodCallHandler {
   private ScanBroadcastReceiver scanBroadcastReceiver;
   private IntentFilter intentFilter;
 
-  public ZkcScannerPlugin(Registrar registrar)
+  public ZkcScannerPlugin() {}
+
+  private void init(Context context, BinaryMessenger messenger)
   {
-    context = registrar.context();
-    channel = new MethodChannel(registrar.messenger(), _METHOD_CHANNEL);
+    this.context = context;
+    channel = new MethodChannel(messenger, _METHOD_CHANNEL);
     channel.setMethodCallHandler(this);
-    init();
-    listeners();
+    
+    scanBroadcastReceiver = new ScanBroadcastReceiver();
+    intentFilter = new IntentFilter();
+    intentFilter.addAction(Constants.ACTION_SCAN_CODE_CALLBACK);
   }
 
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     try
     {
       switch(call.method){
@@ -132,17 +159,6 @@ public class ZkcScannerPlugin implements MethodCallHandler {
   public void onError(Exception error)
   {
     channel.invokeMethod(_ON_ERROR, error.getMessage());
-  }
-
-  private void init()
-  {
-    scanBroadcastReceiver = new ScanBroadcastReceiver();
-    intentFilter = new IntentFilter();
-    intentFilter.addAction(Constants.ACTION_SCAN_CODE_CALLBACK);
-  }
-
-  private void listeners()
-  {
   }
 
   public boolean startScanner()
